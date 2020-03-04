be1e5fbaf3ddde130a2b96ea54a7e21ffbed42d3
testheader <vdlgeert@gmail.com>
2020-03-04 20:37:24 +0100

    fix end-to-end without waits

--- cypress/integration/reacting-to-xhr/my-first-test.spec.js
+++ cypress/integration/reacting-to-xhr/my-first-test.spec.js
@@ -18,14 +18,7 @@ describe('request new account', function() {
     */
 
    it('works end to end', function() {
-    cy.visit('https://www.hbvl.be/?origin=app');
-    
-    cy.server({
-        timeout: 10000,
-    });
-    
-    cy.route('POST', '/api/v1.1/Mail/ConfirmNew').as('newAccount');
-    
+    cy.visit('https://preview.hbvl.be/?origin=app');    
     cy.get('.article-teaser--plus a')
       .first()
       .then(url => navigateToFirstPlusArticle(url));
@@ -34,8 +27,7 @@ describe('request new account', function() {
       cy.get('[data-current-screen="CtaAuthPaying"]').get('form button').click();
       cy.get('[name="emailAddress"]').type("fakemailadres@notrealy.com");
       cy.get('[data-current-screen="Auth10"]').get('button[type="submit"]').click();
-      cy.wait('@newAccount');
-      cy.get('data-current-screen="Conf20"').should('exist');
+      cy.get('[data-current-screen="Conf20"]').should('exist');
   }) 
    
 })
